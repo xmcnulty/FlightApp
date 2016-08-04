ac9452632565177bedc641eff8e96ac662d9a0e9
xmcnulty <x.mcnulty@gmail.com>
2016-08-03 21:33:52 -0400

    Redoing autocomplete, creating a custom autocomplete screen.

--- FlightApp/ViewController.swift
+++ FlightApp/ViewController.swift
@@ -10,18 +10,6 @@ import UIKit
 import GoogleMaps
 
 class ViewController: UIViewController {
-
-    // used to select originating city
-    @IBOutlet weak var originEntry: UITextField!
-    
-    // when the user wants to select the origin airport,
-    // a GMSAutocomplete window will be displayed to assist the 
-    // user in selecting an airport.
-    @IBAction func originSelected(sender: AnyObject) {
-        let autocompleteController = GMSAutocompleteViewController()
-        autocompleteController.delegate = self
-        self.presentViewController(autocompleteController, animated: true, completion: nil)
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,39 +22,4 @@ class ViewController: UIViewController {
     }
 
 
-}
-
-extension ViewController: GMSAutocompleteViewControllerDelegate {
-    
-    // Handle the user's selection.
-    func viewController(viewController: GMSAutocompleteViewController, didAutocompleteWithPlace place: GMSPlace) {
-        print("Place name: ", place.name)
-        print("Place address: ", place.formattedAddress)
-        print("Place attributions: ", place.attributions)
-        
-        // set the text of the origin text box
-        originEntry.text = place.name
-        
-        self.dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    func viewController(viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: NSError) {
-        // TODO: handle the error.
-        print("Error: ", error.description)
-    }
-    
-    // User canceled the operation.
-    func wasCancelled(viewController: GMSAutocompleteViewController) {
-        self.dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    // Turn the network activity indicator on and off again.
-    func didRequestAutocompletePredictions(viewController: GMSAutocompleteViewController) {
-        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
-    }
-    
-    func didUpdateAutocompletePredictions(viewController: GMSAutocompleteViewController) {
-        UIApplication.sharedApplication().networkActivityIndicatorVisible = false
-    }
-    
 }
